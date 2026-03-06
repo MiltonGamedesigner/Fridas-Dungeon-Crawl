2207c786fda3b83d4b176ceefb7b503ea2b4ad99
jojful <fstenfeldt@gmail.com>
2026-03-06 10:01:58 +0100

    camera (FIRE)

--- Dungeon Crawl/Assets/Scripts/CameraMovement.cs
+++ Dungeon Crawl/Assets/Scripts/CameraMovement.cs
@@ -1,20 +1,85 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    int cameraDir;
+    public CinemachineCamera camN;
+    public CinemachineCamera camE;
+    public CinemachineCamera camS;
+    public CinemachineCamera camW;
+
     void Start()
     {
-        
+        cameraDir = 1;
+        camN.Priority = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D))
+        if (cameraDir == 1)
         {
-
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                camN.Priority = 3;
+                camE.Priority = 10;
+                cameraDir = 2;
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                camN.Priority = 3;
+                camW.Priority = 10;
+                cameraDir = 4;
+            }
         }
+        else if (cameraDir == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                camE.Priority = 2;
+                camS.Priority = 10;
+                cameraDir = 3;
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                camE.Priority = 3;
+                camN.Priority = 10;
+                cameraDir = 1;
+            }
+        }
+        else if (cameraDir == 3)
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                camS.Priority = 1;
+                camW.Priority = 10;
+                cameraDir = 4;
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                camS.Priority = 3;
+                camE.Priority = 10;
+                cameraDir = 2;
+            }
+        }
+        else if (cameraDir == 4)
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                camW.Priority = 0;
+                camN.Priority = 10;
+                cameraDir = 1;
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                camW.Priority = 3;
+                camS.Priority = 10;
+                cameraDir = 3;
+            }
+        }
+
+        
 
     }
 }
