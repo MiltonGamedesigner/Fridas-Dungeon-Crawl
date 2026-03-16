96595beab6fe5677be97c3b3c38e6fb518893d89
jojful <fstenfeldt@gmail.com>
2026-03-16 12:16:50 +0100

    Move Forward

--- Dungeon Crawl/Assets/Fridas Scripts/PlayerMovement.cs
+++ Dungeon Crawl/Assets/Fridas Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     bool isMovingForward = false;
+    float moveTimer = 0;
 
 
     void Start()
@@ -13,21 +14,28 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * 1 * Time.deltaTime;
+        //transform.position += transform.forward * 1 * Time.deltaTime;
         if (isMovingForward == true)
         {
-            transform.position += transform.forward * 1 * Time.deltaTime;
+            transform.position += transform.forward * 7 * Time.deltaTime;
+            moveTimer -= Time.deltaTime;
+
+            if (moveTimer <= 0)
+            {
+                isMovingForward = false;
+            }
         }
     }
 
     public void ExitRoom()
     {
+        moveTimer = 0.6f;
         isMovingForward = true;
 
-        while (isMovingForward == true)
-        {
-            transform.position += transform.forward * 1 * Time.deltaTime;
-        }
+        //while (isMovingForward == true)
+        //{
+        //    transform.position += transform.forward * 1 * Time.deltaTime;
+        //}
         
     }
 }
